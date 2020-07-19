6c361d9f41ca15d504c6345add870ef4a2ab3f41
Kashish121 <56366103+Kashish121@users.noreply.github.com>
2020-07-20 03:14:37 +0530

    rooms created

--- public/script.js
+++ public/script.js
@@ -1,19 +1,20 @@
 const socket = io('http://localhost:3000', { transports: ['websocket'] })
 const messageForm = document.getElementById('send-container')
 const messageContainer = document.getElementById('message-container')
+const roomContainer = document.getElementById('room-container')
 const messageInput = document.getElementById('message-input')
 
 if (messageForm != null) {
     const name = prompt('What should we call you?')
     appendMessage(`${name} you joined`)
-    socket.emit('new-user', name)
+    socket.emit('new-user', roomName, name)
 
     messageForm.addEventListener('submit', e => {
         e.preventDefault()
             // prevents refreshing the page every time, *avoids losing conversation history*
         const message = messageInput.value
         appendMessage(`You: ${message}`)
-        socket.emit('send-chat-message', message)
+        socket.emit('send-chat-message', roomName, message)
         messageInput.value = ''
             // empties message input box after the message is sent
     })
@@ -27,6 +28,7 @@ socket.on('room-created', room => {
     const roomLink = document.createElement('a')
     roomLink.href = `/${room}`
     roomLink.innerText = 'join'
+    roomContainer.append(roomLink)
 })
 
 socket.on('chat-message', data => {
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
-const rooms = { name: {} }
+const rooms = {}
 
 app.get('/', (req, res) => {
     res.render('index', { rooms: rooms })
@@ -32,19 +32,20 @@ app.get('/:room', (req, res) => {
 
 server.listen(3000)
 
-const users = {}
+//const users = {}
 
 io.on('connection', socket => {
 
-    socket.on('new-user', name => {
-        users[socket.id] = name
-        socket.broadcast.emit('user-connected', name)
+    socket.on('new-user', (room, name) => {
+        socket.join(room)
+        rooms[room].users[socket.id] = name
+        socket.to(room).broadcast.emit('user-connected', name)
     })
-    socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    socket.on('send-chat-message', (room, message) => {
+        socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
     })
     socket.on('disconnect', () => {
-        socket.broadcast.emit('user-disconnected', users[socket.id])
-        delete users[socket.id]
+        socket.broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+        delete rooms[room].users[socket.id]
     })
 })
